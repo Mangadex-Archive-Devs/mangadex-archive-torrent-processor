dcd9234659492cb46392547f9927f905e0d7374e
radonthetyrant <radon@rn86.de>
2018-04-13 16:36:34 +0200

    added anidex upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const createTorrent = require('create-torrent');
 const fs = require('fs');
+const FormData = require('form-data');
 const torrent_directory = './';
 
 module.exports = {
@@ -56,10 +57,43 @@ module.exports = {
     /*
       torrent_file_path,    //Where the .torrent-file is located at
       anidex_description,   //Description for anidex
-      anidex_hentai         //Is the manga considered adult content
+      anidex_hentai,         //Is the manga considered adult content
+      anidex_subcat_id,
+      anidex_api_key,
+      anidex_private,
+      anidex_debug
     */
 
     //Upload to anidex
-    //TODO
+
+    let form = new FormData({});
+    form.append('file', fs.createReadStream(arguments.torrent_file_path));
+    form.append('subcat_id', arguments.anidex_subcat_id);
+    form.append('group_id', 0); // no group
+    form.append('lang_id', 1); // English
+    form.append('description', arguments.anidex_description);
+    form.append('batch', 1);
+    form.append('hentai', arguments.anidex_hentai);
+    form.append('api_key', arguments.anidex_api_key);
+    form.append('debug', arguments.anidex_debug);
+    form.append('private', arguments.anidex_private);
+    form.submit('https://anidex.info/api/', function (err, res) {
+        if (err) {
+            console.error("API upload error:", err);
+            cb(false);
+        } else {
+            let data = "";
+            res.resume()
+                .on('data', (chunk) => {
+                    data += chunk;
+                })
+                .on('end', () => {
+                    //console.log("API upload response:", res.statusCode, res.body, data);
+                    let rx = /https:\/\/anidex\.info\/torrent\/(\d+)/i;
+                    let arr = rx.exec(data);
+                    cb(arr && arr[1] != null ? parseInt(arr[1]) : -1);
+                });
+        }
+    });
   }
-}
+};
